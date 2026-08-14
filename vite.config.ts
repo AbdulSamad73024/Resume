4ad6f214344d0db56b5fc297b127e9fcf15ed743
AbdulSamad73024 <as7302416108@gmail.com>
2026-08-14 23:41:57 +0530

    Configure GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import {defineConfig} from 'vite';
 export default defineConfig(() => {
   return {
     plugins: [react(), tailwindcss()],
+     base: '/Resume/',
     resolve: {
       alias: {
         '@': path.resolve(__dirname, '.'),
